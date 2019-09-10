495f0200869f4b25d5ece79eebc71153c7d9a211
Dan Ran <randanfx@gmail.com>
2019-09-10 00:06:08 -0400

    sketchupdate

--- p1/fetch.js
+++ p1/fetch.js
@@ -7,12 +7,18 @@ const IslamNFashionUrl = 'https://collectionapi.metmuseum.org/public/collection/
 const ChineseArtUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=6"
 
 //search needs contain a query.
-const ChineseArtUrl2 = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=Painting"
-
-fetchMuseumData(ChineseArtUrl);
+const ChineseArtUrl2 = "https://collectionapi.metmuseum.org/public/collection/v1/search?geoLocation=China&q=dog"
 
 let metData;
 let myArray = [];
+
+fetchMuseumData(ChineseArtUrl2);
+
+
+
+// var json = ;
+// var fs = require('fs');
+// fs.writeFile('metDataTest.json', JSON.stringigy(myArray), 'utf8', callback);
 
 // fetch a query
 function fetchMuseumData(url) {
@@ -22,30 +28,59 @@ function fetchMuseumData(url) {
     .then(data => {
       console.log(data);
       fetchObjects(data);})
-    .then(data => {
-      console.log(myArray);
-    });
+
+    .then(function(data){
+       // console.log(data)
+       // createImg()
+      })
+      ;
+
+      // fs.writeFile('metDataTest.json', JSON.stringigy(myArray), 'utf8', callback);
+
+
 
 
 }
 
+
+function createImg(){
+  console.log(myArray['0']);
+  var container = document.getElementById('imageContainer');
+  var docFrag = document.createDocumentFragment();
+    myArray.forEach(function(obj,index,originalArray){
+        var img = document.createElement('img');
+        img.src = obj.image;
+
+        docFrag.appendChild(img);
+      });
+
+}
+
+
+
 // from the response, fetch objects
 function fetchObjects(data){
 
-
-
+  var itemProcessed =0;
   let objectIDs = data.objectIDs.slice(0,20);
   console.log("fetching: " + objectIDs.length + " objects");
-    objectIDs.forEach(function(n) {
-      console.log(objectBaseUrl + n);
+    objectIDs.forEach(function(n,i,a) {
+      // console.log(objectBaseUrl + n);
+      itemProcessed++;
       let objUrl = objectBaseUrl + n;
       window
         .fetch(objUrl)
         .then(data => data.json())
         .then(data => {
           // console.log(data);
+          // console.log(data.department)
           addObject(data);
         });
+
+        if(itemProcessed ==a.length){
+          createImg();
+        }
+
     });
 
 }
@@ -74,7 +109,7 @@ function addObject(objectData){
       "classification" : objectData.classification
 
     });
-    console.log(objectData.culture);
+    // console.log(objectData.culture);
 
 
 }
